0bd62961009d2b2dfe9d579b8cc1e702a67f3634
Anuran Roy <76481787+anuran-roy@users.noreply.github.com>
2026-06-14 01:26:18 +0530

    Solve max-of-array

---
+++ study-plans/cuda-basics/max-of-array/max-of-array.cu
@@ -0,0 +1,63 @@
+#include <cuda_runtime.h>
+#include <float.h>
+#include <math.h>
+
+__global__ void max_kernel(const float* input, float* partial, int N) {
+    // Write code here
+    int globalIdx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    __shared__ float partial_maxes[256];
+
+    float max_val = -INFINITY;
+
+    for(int i = globalIdx; i < N; i += blockDim.x * gridDim.x) {
+        max_val = fmaxf(max_val, input[i]);
+    }
+
+    partial_maxes[threadIdx.x] = max_val;
+    __syncthreads();
+
+    // Now we go to tree reduction
+
+    for (int stride = blockDim.x/2 ; stride > 0; stride /= 2) {
+        if (threadIdx.x < stride) {
+            partial_maxes[threadIdx.x] = fmaxf(partial_maxes[threadIdx.x], partial_maxes[threadIdx.x + stride]);
+        }
+
+        __syncthreads();
+    }
+
+    if (threadIdx.x == 0) {
+        partial[blockIdx.x] = partial_maxes[0];
+    }
+}
+
+__global__ void final_max_kernel(const float* partial, float* result, int blocks) {
+    __shared__ float sdata[256];
+    int tid = threadIdx.x;
+
+    sdata[tid] = (tid < blocks) ? partial[tid] : -INFINITY;
+    __syncthreads();
+
+    for (int stride = blockDim.x / 2; stride > 0; stride /= 2) {
+        if (tid < stride)
+            sdata[tid] = fmaxf(sdata[tid], sdata[tid + stride]);
+        __syncthreads();
+    }
+
+    if (tid == 0) *result = sdata[0];
+}
+
+extern "C" void solve(const float* input, float* result, int N) {
+    int threads = 256;
+    int blocks = (N + threads - 1) / threads;
+
+    float* d_partial;
+    cudaMalloc(&d_partial, blocks * sizeof(float));
+
+    max_kernel<<<blocks, threads>>>(input, d_partial, N);
+    final_max_kernel<<<1, 256>>>(d_partial, result, blocks);
+
+    cudaDeviceSynchronize();
+    cudaFree(d_partial);
+}
